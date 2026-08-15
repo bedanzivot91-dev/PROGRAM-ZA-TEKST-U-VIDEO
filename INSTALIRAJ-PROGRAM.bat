6e67fb78f2ba8b60477e94a4f6ed239093f3e4e6
bedanzivot91-dev <bedanzivot91@gmail.com>
2026-08-15 23:10:08 +0200

    Fix installer launcher to run existing setup EXE

--- INSTALIRAJ-PROGRAM.bat
+++ INSTALIRAJ-PROGRAM.bat
@@ -1,2 +1,23 @@
 @echo off
-call "%~dp0SASTAVI-PROGRAM.bat"
+setlocal EnableExtensions
+cd /d "%~dp0"
+title Instalacija Muzicki Spot Studio Free
+
+set "INSTALLER=%~dp0INSTALLER\Muzicki-Spot-Studio-Free-Setup-v15.6.0.exe"
+set "BUILDER=%~dp0SASTAVI-PROGRAM.bat"
+
+if exist "%INSTALLER%" (
+  echo Pokrecem installer...
+  start "" /wait "%INSTALLER%"
+  exit /b %ERRORLEVEL%
+)
+
+if exist "%BUILDER%" (
+  call "%BUILDER%"
+  exit /b %ERRORLEVEL%
+)
+
+echo GRESKA: Installer nije pronadjen.
+echo Ocekivana putanja: %INSTALLER%
+pause
+exit /b 1
