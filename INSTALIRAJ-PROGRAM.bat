1486a5d5fcbfd3ae933265ff768f58bd22e775fe
bedanzivot91-dev <bedanzivot91@gmail.com>
2026-09-15 06:29:26 +0200

    Make installer launcher always use newest available version

--- INSTALIRAJ-PROGRAM.bat
+++ INSTALIRAJ-PROGRAM.bat
@@ -1,23 +1,26 @@
 @echo off
-setlocal EnableExtensions
+setlocal EnableExtensions EnableDelayedExpansion
 cd /d "%~dp0"
 title Instalacija Muzicki Spot Studio Free
 
-set "INSTALLER=%~dp0INSTALLER\Muzicki-Spot-Studio-Free-Setup-v15.6.0.exe"
+set "INSTALLER_DIR=%~dp0INSTALLER"
+set "INSTALLER="
+for /f "delims=" %%F in ('dir /b /a:-d /o:-n "%INSTALLER_DIR%\Muzicki-Spot-Studio-Free-Setup-v*.exe" 2^>nul') do if not defined INSTALLER set "INSTALLER=%INSTALLER_DIR%\%%F"
+
+if defined INSTALLER (
+  echo Pokrecem najnoviji pronadjeni installer:
+  echo !INSTALLER!
+  start "" /wait "!INSTALLER!"
+  exit /b !ERRORLEVEL!
+)
+
 set "BUILDER=%~dp0SASTAVI-PROGRAM.bat"
-
-if exist "%INSTALLER%" (
-  echo Pokrecem installer...
-  start "" /wait "%INSTALLER%"
-  exit /b %ERRORLEVEL%
+if exist "!BUILDER!" (
+  call "!BUILDER!"
+  exit /b !ERRORLEVEL!
 )
 
-if exist "%BUILDER%" (
-  call "%BUILDER%"
-  exit /b %ERRORLEVEL%
-)
-
-echo GRESKA: Installer nije pronadjen.
-echo Ocekivana putanja: %INSTALLER%
+echo GRESKA: Installer niti skripta za sastavljanje nisu pronadjeni.
+echo Ocekivan folder: !INSTALLER_DIR!
 pause
 exit /b 1
